f0a3a81498bbd9165c1f8ac65ffd06d626445751
木原大輝 <recode.seadex777@gmail.com>
2018-11-07 12:30:49 +0900

    refactor: 初期作成されたファイルの調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       :mini-variant="miniVariant"
       :clipped="clipped"
@@ -29,53 +29,15 @@
       fixed
       app
     >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+      <v-toolbar-side-icon @click="drawer = !drawer"/>
       <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer
       :fixed="fixed"
       app
@@ -86,20 +48,47 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [
-        { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-      ],
+      clipped: true,
+      drawer: false,
+      fixed: true,
+      items: [{ icon: 'apps', title: 'Welcome', to: '/' }],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: '麻生OCチャット'
+    }
+  },
+
+  created() {
+    this.setLoginNavItem()
+  },
+
+  watch: {
+    getIsAuth() {
+      this.items.pop()
+      this.setLoginNavItem()
+    }
+  },
+
+  computed: {
+    ...mapGetters('auth', ['getIsAuth'])
+  },
+
+  methods: {
+    setLoginNavItem() {
+      const loginState = {}
+      if (this.getIsAuth) {
+        this.$set(loginState, 'icon', 'lock')
+        this.$set(loginState, 'title', 'ログアウト')
+        this.$set(loginState, 'to', '/sign-out')
+      } else {
+        this.$set(loginState, 'icon', 'lock_open')
+        this.$set(loginState, 'title', 'ログイン')
+        this.$set(loginState, 'to', '/sign-in')
+      }
+      this.items.push(loginState)
     }
   }
 }
